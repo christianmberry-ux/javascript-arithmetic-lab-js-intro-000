4177d86098cc7ad65eb3f7740f8011de4ea22508
Christian Berry <christianmatthewberry@gmail.com>
2020-04-13 20:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function add() {
-  number++
+function add(a,b) {
+  a + b
+}
+
+function subtract(a,b) {
+  a - b
+}
+
+function multiply(a,b) {
+  a * b
+}
+
+function divide(a,b) {
+  a / b
+}
+
+function increment(n) {
+  n += 1
+}
+
+function decrement(n) {
+  n -= 1
+}
+
+function makeInt(string) {
+  parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  parseFloat(string)
 }
